4455fe2395f5cd900e023e9c5804104a6b948bb1
mielindev <phamvietquang.dev@gmail.com>
2024-09-23 10:52:30 +0700

    completely layout

--- customer/controller/index.js
+++ customer/controller/index.js
@@ -12,6 +12,27 @@ axios({
     console.log("👉 ~ err:", err);
   });
 // render product
-function renderProduct(phone) {
-  console.log("👉 ~ renderProduct ~ phone:", phone);
+function renderProduct(phoneList) {
+  let content = "";
+  phoneList.forEach((phone) => {
+    console.log("👉 ~ phoneList.forEach ~ phone:", phone);
+    return (content += `
+      <div class="col">
+        <div class="card shadow-2xl">
+            <img src="${phone.img}" class="card-img-top w-50" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title text-2xl">${phone.name}</h5>
+              <p class="card-text">
+                ${phone.desc}
+              </p>
+              <div class="buttonCard d-flex justify-content-between">
+                <button href="#">Buy Now</button>
+                <button href="#">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+      </div>
+    `);
+  });
+  document.querySelector(".product_body .row").innerHTML = content;
 }
